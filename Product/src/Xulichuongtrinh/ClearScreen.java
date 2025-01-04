253be5a84fdd826e622b6852b0cf3eaa503c4a99
Toanproptit <nguyentrongtoana1byt@gmail.com>
2025-01-04 23:43:48 +0700

    btgk

---
+++ Product/src/Xulichuongtrinh/ClearScreen.java
@@ -0,0 +1,9 @@
+package Xulichuongtrinh;
+
+public class ClearScreen{
+    public static void clearScreen() {
+        for(int i=0;i<30;++i){
+            System.out.println("");
+        }
+    }
+}
